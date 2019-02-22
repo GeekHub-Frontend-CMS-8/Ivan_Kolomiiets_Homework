d5fe6b9fef29751ee2c973eefecdbed71d74f48e
maretes <kolom100@gmail.com>
2019-02-22 22:44:11 +0200

    new modified

--- HW-16/build/js/open_popup.js
+++ HW-16/build/js/open_popup.js
@@ -7,3 +7,13 @@ function openblock(idblock) {
         document.getElementById(idblock).style.display = "none"; // переключаем свойство в none
     }
 }
+
+function openpop(idpop) {
+    if (document.getElementById(idpop).style.display == "none") {
+        /*Если свойство display равно none*/
+        document.getElementById(idpop).style.display = "block"; // переключаем свойство в block
+    } else {
+        /*Если свойство display не равно none*/
+        document.getElementById(idpop).style.display = "none"; // переключаем свойство в none
+    }
+}
